dd510fc11d9bc61c89c9d05ebf0597383d821a70
Arthur Braga <programadorbraga@gmail.com>
2024-09-10 22:41:27 -0300

    More opengl
    Need to see about EBO

--- opengl/learn/part_1/NOTES.md
+++ opengl/learn/part_1/NOTES.md
@@ -1,13 +1,41 @@
-# Technically, OpenGL is merely a specification, developed and maintained by khronos.org
+# Part 1 notes
 
-# The OpenGL specification specifies exactly what the result/output of each function should be and how it should perform
+- Technically, OpenGL is merely a specification, developed and maintained by khronos.org
 
-# Most implementations of OpenGL are built by graphics card manufactures
+- The OpenGL specification specifies exactly what the result/output of each function should be and how it should perform
 
-# OpenGL is by itself a large state machine
+- Most implementations of OpenGL are built by graphics card manufactures
 
-# An object in OpenGL is a collection of options that represents a subset of OpenGL's state
+- OpenGL is by itself a large state machine
 
-# GLFW can be used to create an OpenGL context, define a window parameters, and handle user input
+- An object in OpenGL is a collection of options that represents a subset of OpenGL's state
 
-#
+- GLFW can be used to create an OpenGL context, define a window parameters, and handle user input
+
+- The major part of OpenGL work is to convert 3D coordinates to 2D pixels that fit on screen. This process is manage by process pipeline
+
+- The graphics pipeline can be divided into several steps and, because the parallel nature of this process, graphics cards of today have thousand of small processing cores to quickly process your data within the graphics pipeline, where each one of small programs that run in those cores are called shaders.
+
+- Shaders are written in OpenGL Shading Language (GLSL)
+
+- We can crate vertex, geometry and fragment shaders
+
+- A vertex is a collection of data per 3D coordinate. This vertex's data is represented using vertex attributes
+
+- The vertex shader takes as input a single vertex and his main purpose is to transform 3D coordinates into different 3D coordinates
+
+- The primitive assembly stage takes as input all the vertices from the vertex shader that form a primitive and assembles all the points in the primitive shape given. The output is passed to the geometry shader
+
+- The geometry shader takes as input a collection of all vertices that form a primitive and has the ability to generate other shapes by emitting new vertices to form new (or other) primitive(s). The output is then passed on the rasterization stage, where it maps the resulting primitive(s) to the corresponding pixels on the final screen, resulting in fragment for the fragment shader to use
+
+- Before the fragment shader run, all fragment that are outside our views are discarted (clipping)
+
+- The main purpose of fragment shader is to calculate the final color of a pixel
+
+- The final object will then pass to through one more stage called alpha test and blending test, where the corresponding depth (and stencil) value of the fragment and uses those to check if the resulting fragment is in front or behind other objects and should be discarded accordingly. The stage also checks for alpha values (alpha values define the opacity of an object) and blends the objects accordingly.
+
+- Coordinates must be in NDC (normalized device coordinates)
+
+- VBO(vertex buffer objects) can store a large number of vertices in GPU's memory
+
+-
--- opengl/learn/part_1/src/main.cpp
+++ opengl/learn/part_1/src/main.cpp
@@ -1,6 +1,7 @@
 #include <glad/gl.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <fstream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
   glViewport(0, 0, width, height);
@@ -9,6 +10,106 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 void processInput(GLFWwindow *window) {
   if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
     glfwSetWindowShouldClose(window, true);
+}
+
+std::string readShader(const std::string& shaderPath) {
+  std::ifstream shaderFile(shaderPath);
+
+  if (!shaderFile.is_open()) {
+    std::cerr << "Error reading shader!" << std::endl;
+    return "";
+  }
+  
+  std::string shaderLine;
+  std::string shaderContent;
+
+  while (getline(shaderFile, shaderLine)) {
+    shaderContent += shaderLine + "\n";
+  }
+  
+  shaderFile.close();
+  
+  std::cout << "Shader content: " << shaderContent << std::endl;
+
+  return shaderContent;
+}
+
+unsigned int createShader(const std::string& shaderPath, const GLenum& shaderType) {
+  const std::string shaderContent = readShader(shaderPath);
+  const char * shaderContentCStr = shaderContent.c_str();
+  
+  if (shaderContent == "") {
+    return -1;
+  }
+
+  unsigned int shader = glCreateShader(shaderType);
+  glShaderSource(shader, 1, &shaderContentCStr, nullptr);
+  glCompileShader(shader);
+
+  int isCompiled;
+  char infoLog[512];
+  glGetShaderiv(shader, GL_COMPILE_STATUS, &isCompiled);
+
+  if (!isCompiled) {
+    glGetShaderInfoLog(shader, 512, nullptr, infoLog);
+    std::cerr << "Error compiling shader!" << std::endl;
+    std::cerr << "Error: " << std::endl << infoLog << std::endl;
+    return -1;
+  }
+
+  else {
+    std::cout << "Shader compiled!" << std::endl;
+  }
+
+  return shader;
+}
+
+unsigned int returnTriangleShader() {
+  float vertices[] = {
+    -1.0f, -1.0f, 0.0f,
+    0.0f, 1.0f, 0.0f,
+    1.0f, -1.0f, 0.0f
+  };
+  
+  unsigned int VBO;
+  glGenBuffers(1, &VBO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+
+  unsigned int vertexShader = createShader("../shaders/vertex_shader.glsl", GL_VERTEX_SHADER);
+  unsigned int fragmentShader = createShader("../shaders/fragment_shader.glsl", GL_FRAGMENT_SHADER);
+  
+  if (vertexShader == -1 || fragmentShader == -1) {
+    return -1;
+  }
+
+  unsigned int shaderProgram = glCreateProgram();
+
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+
+  char linkedLog[512];
+  int isLinked;
+  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &isLinked);
+
+  if (!isLinked) {
+    glGetProgramInfoLog(shaderProgram, 512, nullptr, linkedLog);
+
+    std::cerr << "Error linking shaders: " << std::endl << linkedLog << std::endl;
+  
+    return -1;
+  }
+  else {
+    std::cout << "Shaders linked!" << std::endl;
+  }
+
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+
+  return shaderProgram;
 }
 
 int main() {
@@ -29,17 +130,29 @@ int main() {
 
   if (!gladLoadGL(glfwGetProcAddress)) {
     std::cout << "Failed to initialize GLAD" << std::endl;
+    glfwTerminate();
+    return 1;
   }
 
   glViewport(0, 0, 800, 600);
   
   glfwSetFramebufferSizeCallback(w, framebuffer_size_callback);
 
-  while(!glfwWindowShouldClose(w)) {
+  unsigned int VAO;
+  glGenVertexArrays(1, &VAO);
+  glBindVertexArray(VAO);
+  
+  unsigned int triangleShader = returnTriangleShader();
+
+  while(!glfwWindowShouldClose(w) && triangleShader  != -1) {
     processInput(w);
     
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+    
+    glUseProgram(triangleShader);
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
     glfwSwapBuffers(w);
     glfwPollEvents();
@@ -49,5 +162,3 @@ int main() {
 
   return 0;
 }
-
-
